c6190e54a2461f8492bd57de60dd0c6cbc25453f
Akshit Trikha <akshittrikha01@gmail.com>
2024-02-20 00:07:07 +0530

    Fix: Moved to nodemon for hot reload. Add: Upload to Cloudfare R2 functionality. Todo: Initialize Redis to push the ids.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
-
-import { filePaths, generate } from "./utils.js";
 import path from "path";
+
+import { client, getBucket, upload } from "./s3";
+import { filePaths, generate } from "./utils.js";
 
 const PORT = 3000;
 
@@ -24,13 +25,21 @@ app.post("/upload", async (req, res) => {
     const id = generate();
     const git = simpleGit();
     await git.clone(repoURL, `out/${id}`);
-    
-    const files:string[] | undefined | null = filePaths(path.join(process.cwd(), `out/${id}`));
-    files?.forEach((file:string) => {
-        console.log(path.resolve(`out/${id}`, file));
-    });
 
-    res.send({ id: id });
+    const files: string[] | undefined | null = filePaths(path.join(process.cwd(), `out/${id}`));
+
+    try {
+        const bucket = await getBucket(client);
+
+        const putResponse = await upload(client, id, files, bucket);
+
+        return res.status(200).send({ id: id, message: "Upload Successful", putResponse: putResponse });
+
+    } catch (err) {
+        console.error("Fetching Bucket list error: ", err);
+        return res.status(403).send({ message: "Error fetching bucket list." });
+    }
+
 })
 
 app.listen(PORT, () => {
