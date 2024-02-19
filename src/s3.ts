f0b26493a4568837beee05ff50a0b22e1c98c623
Akshit Trikha <akshittrikha01@gmail.com>
2024-02-20 00:07:07 +0530

    Fix: Moved to nodemon for hot reload. Add: Upload to Cloudfare R2 functionality. Todo: Initialize Redis to push the ids.

---
+++ src/s3.ts
@@ -0,0 +1,46 @@
+import { ListBucketsCommand, S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+import path from "path"
+
+// @ts-ignore
+export const client = new S3Client({
+    region: "apac",
+    endpoint: process.env.CF_S3_ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.CF_ACCESS_KEY_ID,
+        secretAccessKey: process.env.CF_SECRET_ACCESS_KEY
+    }
+})
+
+export async function getBucket(s3Client: S3Client) {
+    try {
+        return (await s3Client.send(new ListBucketsCommand("")))?.Buckets?.[0].Name;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function upload(id: string, files: string[], bucket: any) {
+    let filesToUpload = files.filter((file) => { return file.includes(".git") === false });       //! Removing .git files for testing. Revert this change.
+
+    try {
+        filesToUpload.forEach(async (file) => {
+            const stat = fs.statSync(path.resolve(`out/${id}`, file));
+            let input;
+            if (stat.isFile()) {
+                input = {
+                    Key: file.split(path.sep).join(path.posix.sep),
+                    Body: file,
+                    Bucket: bucket
+                }
+            } else {
+                input = {
+                    Key: file.split(path.sep).join(path.posix.sep) + "/",
+                    Bucket: bucket
+                }
+            }
+        })
+    } catch (err) {
+        throw err;
+    }
+}
